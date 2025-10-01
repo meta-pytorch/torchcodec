1aebfec4a976b1c91b9595b3f1da02fb190bf952
Daniel Flores <danielflores3@fb.com>
2025-10-01 10:18:18 -0400

    crf default

--- src/torchcodec/_core/ops.py
+++ src/torchcodec/_core/ops.py
@@ -259,7 +259,7 @@ def encode_video_to_file_abstract(
     frames: torch.Tensor,
     frame_rate: int,
     filename: str,
-    crf: Optional[int],
+    crf: Optional[int] = None,
 ) -> None:
     return
 
