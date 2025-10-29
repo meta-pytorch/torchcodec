7ed7f21ac6dcd91324c33df4963ecca257a79a0b
Dan-Flores <danielflores3@meta.com>
2025-10-29 13:54:25 -0400

    incorporate feedback

--- src/torchcodec/encoders/_video_encoder.py
+++ src/torchcodec/encoders/_video_encoder.py
@@ -14,21 +14,16 @@ class VideoEncoder:
         frames (``torch.Tensor``): The frames to encode. This must be a 4D
             tensor of shape ``(N, C, H, W)`` where N is the number of frames,
             C is 3 channels (RGB), H is height, and W is width.
-            A 3D tensor of shape ``(C, H, W)`` is also accepted as a single RGB frame.
             Values must be uint8 in the range ``[0, 255]``.
-        frame_rate (int): The frame rate to use when encoding the
-            **input** ``frames``.
+        frame_rate (int): The frame rate of the **input** ``frames``. Also defines the encoded **output** frame rate.
     """
 
     def __init__(self, frames: Tensor, *, frame_rate: int):
         torch._C._log_api_usage_once("torchcodec.encoders.VideoEncoder")
         if not isinstance(frames, Tensor):
             raise ValueError(f"Expected frames to be a Tensor, got {type(frames) = }.")
-        if frames.ndim == 3:
-            # make it 4D and assume single RGB frame, CHW -> NCHW
-            frames = torch.unsqueeze(frames, 0)
         if frames.ndim != 4:
-            raise ValueError(f"Expected 3D or 4D frames, got {frames.shape = }.")
+            raise ValueError(f"Expected 4D frames, got {frames.shape = }.")
         if frames.dtype != torch.uint8:
             raise ValueError(f"Expected uint8 frames, got {frames.dtype = }.")
         if frame_rate <= 0:
@@ -46,7 +41,7 @@ class VideoEncoder:
         Args:
             dest (str or ``pathlib.Path``): The path to the output file, e.g.
                 ``video.mp4``. The extension of the file determines the video
-                format and container.
+                container format.
         """
         _core.encode_video_to_file(
             frames=self._frames,
@@ -61,7 +56,7 @@ class VideoEncoder:
         """Encode frames into raw bytes, as a 1D uint8 Tensor.
 
         Args:
-            format (str): The format of the encoded frames, e.g. "mp4", "mov",
+            format (str): The container format of the encoded frames, e.g. "mp4", "mov",
             "mkv", "avi", "webm", "flv", or "gif"
 
         Returns:
@@ -86,7 +81,7 @@ class VideoEncoder:
                 write mode, etc. Methods must have the following signature:
                 ``write(data: bytes) -> int`` and ``seek(offset: int, whence:
                 int = 0) -> int``.
-            format (str): The format of the encoded frames, e.g. "mp4", "mov",
+            format (str): The container format of the encoded frames, e.g. "mp4", "mov",
                 "mkv", "avi", "webm", "flv", or "gif".
         """
         _core.encode_video_to_file_like(
