1a81658fd1c5f8df43eafd56cad6953749905bdf
Daniel Flores <danielflores3@meta.com>
2025-12-05 16:38:52 -0500

    explicit

--- src/torchcodec/_core/Transform.h
+++ src/torchcodec/_core/Transform.h
@@ -62,7 +62,7 @@ class CropTransform : public Transform {
   CropTransform(const FrameDims& dims, int x, int y);
 
   // Becomes a center crop if x and y are not specified.
-  CropTransform(const FrameDims& dims);
+  explicit CropTransform(const FrameDims& dims);
 
   std::string getFilterGraphCpu() const override;
   std::optional<FrameDims> getOutputFrameDims() const override;
