2ad5d7d8064f38f091d40b07a43814fcb7853930
Molly Xu <mollyxu@fb.com>
2026-01-08 07:18:37 -0800

    fix lint

--- test/conftest.py
+++ test/conftest.py
@@ -38,8 +38,9 @@ def pytest_collection_modifyitems(items):
 
         # For skipif, the marker is always present regardless of whether the
         # condition is True or False, so we must check the actual condition.
-    skipif_condition_is_true = any(skipif_marker.args[0] for skipif_marker in item.iter_markers("skipif"))
-
+        skipif_condition_is_true = any(
+            skipif_marker.args[0] for skipif_marker in item.iter_markers("skipif")
+        )
 
         if in_fbcode():
             # fbcode doesn't like skipping tests, so instead we  just don't collect the test
