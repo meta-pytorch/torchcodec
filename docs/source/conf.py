747428537b44b5d1b7f779fa442b1ff4606bc822
Molly Xu <mollyxu@fb.com>
2026-03-06 14:34:03 -0800

    delete custom css

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -221,8 +221,6 @@ html_theme_options = {
     "show_version_warning_banner": True,
 }
 
-html_css_files = ["css/custom_torchcodec.css"]
-
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
